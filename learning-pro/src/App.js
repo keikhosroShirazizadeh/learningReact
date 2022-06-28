d769081e69928a2a6fdb696098d69624de4186a6
keikhosroShirazizadeh <keikhosro.shirazizade@gmail.com>
2022-06-29 02:28:15 +0430

    initializing project
    initial a react project deleted extra file and tailwindcss for designing

---
+++ learning-pro/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <p className='text-blue-600' >hi this is it as much simple you thought</p>
+    
+    </div>
+  );
+}
+
+export default App;
